2cd8ef562c8c39b3ce63003487c465092131ec1c
ellenjane001 <ellenjane.000001998@gmail.com>
2022-06-28 17:06:14 +0800

    counter

---
+++ pages/components/Img.js
@@ -0,0 +1,11 @@
+export default function Img(props) {
+    return (
+        <>
+            <img
+                className={props.className}
+                src={props.src}
+                alt={props.alt}
+            />
+        </>
+    )
+}
